d1233ba3c89465f1a02386446e2eb68640dcf657
idovgalyov <idovgalyov@4xxi.com>
2017-02-03 21:15:02 +0300

    Global parameters module
    Added new module where we can contain global parameters, e.g. constants and planet physical parameters.
    Main.f95 was modified to be able to create id-matrices for a given planet name (in capitals).
    Example of running> ./axis NEPTUNE
    
    Makefile was modified so as it creates correct executable file.

--- axis/main.f95
+++ axis/main.f95
@@ -1,42 +1,41 @@
-program resonances
-    use resonant_axis
-    implicit none
-    ! a_j — semimajor axe of Jupiter, n_j — mean motion of Jupiter, l_j — mean longitude of Jupiter
-    real, parameter :: a_j = 5.204267, n_j = 0.00145024678779705, l_j = 5.65063905672262e-08
-    real, parameter :: a_s  = 5.204267, n_s = 0.000583991090866933, l_s = 3.74890765513312e-07
-    ! order of resonance
-    integer, parameter :: order = 7
+program resonant_axis_calculator
 
-    real :: a_a, n_a, l_a
-    real :: tmp, tmp2, tmp3, diff
-    integer :: i,j,k
-    real, dimension(6) :: resonance 
-  
-    ! cycle: we consider all possible combinations
-    ! let's take the first element in resonance array as positive integer
-    ! the other 2 elements are from -order to order
-    ! From all subresonances we consider just the main subresonance [i,j,k,0,0,-i-k-j]
-    do i=1,8
-        do j=-order,order
-            k_loop: do k=-order,order
-              diff = (0.0 - i - j - k)
-              resonance = (/ i*1.0, j*1.0, k*1.0, 0.0, 0.0, diff /)
-              ! i,j,k = 0 — no resonance
-              if ( (i == 0) .AND. (j==0) .AND. (k==0) ) then
-                  exit k_loop
-              end if
+use global_parameters
+use resonant_axis
+implicit none
+    integer :: i,j,k,s
+    integer:: m1,m
+    integer, dimension(4) :: resonance
+    character(25)::co
+    integer:: planet_number
 
-              tmp = count_axis_3body(resonance, a_j, n_j, l_j, a_s, n_s, l_s)
+    call get_command_argument(1,co)
+    planet_number=planet_id(trim(co))
+    open(unit=8,file="id_matrix_"//trim(co)//".dat",status='replace')
+    co='(4i4,f23.16)'
 
-              ! Order of resonance should be less than 7 and the resonant semi-axis should not be equal to 0
-              if ( abs(diff) < order .AND. tmp > 0.0 ) then
-                  write (*, 20) resonance, tmp
-              end if
+    do m1=1,max_order
+        do m=-1,-m1-max_order,-1
+            if ( nod(abs(m),m1) /= 1) cycle
+            resonance=(/ m1,m,0,-m1-m /)
+            write(8,co) resonance,&
+                count_axis_2body(resonance,a_pl(planet_number),m_pl(planet_number))
+        enddo
+    enddo
+    close(8)
+    
+contains
 
-            end do k_loop
-        end do
-    end do  
-  
-20 format(3f10.1, 3f10.1, 3f10.4)
-  
-end program
+integer function nod(a,b)
+    integer::a,b,c1,c2,c3,c4
+    c1=a;c2=b
+    do while(c1 /= c2)
+        c3=c1
+        c4=c2
+        c1=min(c3,c4)
+        c2=abs(c3-c4)
+    enddo
+    nod=c1
+end function nod
+
+end
