c823fa4b9fc82ade65969a5b2f3e79afcc0989f9
idovgalyov <idovgalyov@4xxi.com>
2017-02-06 16:27:14 +0300

    Makefile update
    Makefile was modified to be smarter with modules
    It must be easy to add new modules now (requires recordinq new module name once only).
    Also some changes in resonant_axis and global_parameters.

--- axis/global_parameters.f95
+++ axis/global_parameters.f95
@@ -3,24 +3,34 @@ implicit none
 
 real(8),parameter:: pi=dacos(-1d0)
 real(8),parameter:: twopi=pi*2
+real(8),parameter:: gp_k=0.017202098795d0! or 1.720209895d−2 by IERS Conv. 2010
 
 real(8),dimension(9)::a_pl=(/ 0.38709843d0,0.72332102d0,1.00000018d0,&
                               1.52371243d0,5.20248019d0,9.54149883d0,&
                               19.18797948d0,30.06952752d0,39.48686035d0 /)
+! Taken from http://ssd.jpl.nasa.gov/?planet_pos
+! Link case for "Keplerian elements for 3000 BC to 3000 AD"
 
 real(8),dimension(9)::m_pl=(/ &
     1.6601367952719304D-07,2.4478383396645447D-06,3.0404326462685257D-06,&
     3.2271514450538743D-07,9.547919384243222D-04,2.858859806661029D-04,&
     4.3662440433515637D-05,5.151389020535497D-05,7.407407407407407D-09 /)
+! Used: http://ssd.jpl.nasa.gov/?planet_phys_par
 
 integer,parameter:: max_order = 20
 
 contains
 
+
 integer function planet_id(s)
+! Get planet ID
+! Given:
+!   s - planet name IN CAPITALS
+! Returns:
+!   <integer> - ID
     character(*) s
     integer i
-    
+
     select case (s)
         case('MERCURY')
             i=1
--- axis/main.f95
+++ axis/main.f95
@@ -3,20 +3,25 @@ program resonant_axis_calculator
 use global_parameters
 use resonant_axis
 implicit none
+
     integer :: i,j,k,s
     integer:: m1,m
     integer, dimension(4) :: resonance
     character(25)::co
     integer:: planet_number
 
+    ! Get planet name and ID
     call get_command_argument(1,co)
     planet_number=planet_id(trim(co))
     open(unit=8,file="id_matrix_"//trim(co)//".dat",status='replace')
     co='(4i4,f23.16)'
 
+    ! Get id. matrix for a planet bya given maximum order in G_P
     do m1=1,max_order
         do m=-1,-m1-max_order,-1
+            ! Waste already observed cases
             if ( nod(abs(m),m1) /= 1) cycle
+            ! Look for main subresonance
             resonance=(/ m1,m,0,-m1-m /)
             write(8,co) resonance,&
                 count_axis_2body(resonance,a_pl(planet_number),m_pl(planet_number))
@@ -27,7 +32,13 @@ implicit none
 contains
 
 integer function nod(a,b)
+! Find gcd - greatest common divisor
+! Given:
+!   a,b - positive integer numbers
+! Returns:
+!   <integer> - gcd
     integer::a,b,c1,c2,c3,c4
+    
     c1=a;c2=b
     do while(c1 /= c2)
         c3=c1
